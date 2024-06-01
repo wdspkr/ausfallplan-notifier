e65e9267980dd1035f0b04a4289fa68bb46bd86c
Timo Recke <wdspkr@users.noreply.github.com>
2024-06-01 23:04:41 +0200

    Add minio client for config

--- go.mod
+++ go.mod
@@ -2,4 +2,23 @@ module github.com/wdspkr/ausfallplan-notifier
 
 go 1.22
 
-require github.com/joho/godotenv v1.5.1
+require (
+	github.com/joho/godotenv v1.5.1
+	github.com/minio/minio-go/v7 v7.0.70
+)
+
+require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/goccy/go-json v0.10.3 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/klauspost/compress v1.17.8 // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/minio/md5-simd v1.1.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rs/xid v1.5.0 // indirect
+	golang.org/x/crypto v0.23.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	gopkg.in/ini.v1 v1.67.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	"github.com/minio/minio-go/v7"
+	"github.com/minio/minio-go/v7/pkg/credentials"
 	"github.com/wdspkr/ausfallplan-notifier/ausfallplan"
 )
 
@@ -15,10 +19,47 @@ func main() {
 		log.Fatal(err, "Error loading .env file")
 	}
 
+	minioClient := initializeMinioClient()
+
+	object, err := minioClient.GetObject(context.Background(), os.Getenv("S3_BUCKET"), "config.json", minio.GetObjectOptions{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer object.Close()
+
+	// Read the object
+	config, err := io.ReadAll(object)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Print the object
+	fmt.Println(string(config))
+
 	entries := ausfallplan.GetEntriesFor(os.Getenv("LEVEL"), os.Getenv("CLASS"))
 
 	for _, s := range entries {
 		formattedDay := s.Day.Format("02.01.2006")
 		fmt.Printf("%s %s %s %s\n", formattedDay, s.Hour, s.Class, s.Information)
 	}
+}
+
+func initializeMinioClient() *minio.Client {
+	endpoint := os.Getenv("S3_ENDPOINT")
+	accessKeyID := os.Getenv("S3_ACCESS_KEY_ID")
+	secretAccessKey := os.Getenv("S3_SECRET_ACCESS_KEY")
+	useSSL := os.Getenv("S3_IS_LOCAL") != "true"
+
+	minioClient, err := minio.New(endpoint, &minio.Options{
+		Creds:  credentials.NewStaticV4(accessKeyID, secretAccessKey, ""),
+		Secure: useSSL,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return minioClient
 }
